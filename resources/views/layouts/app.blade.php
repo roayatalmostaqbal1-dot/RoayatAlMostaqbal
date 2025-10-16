b39befcce0f134ab6a3295fbfdf007ebdaa0f592
x-dynamite-x <abdmoklss@gmail.com>
2025-10-16 04:39:22 +0300

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&display=swap" rel="stylesheet">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 

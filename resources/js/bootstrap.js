7094d47de01f1176325349655297bc3688a2d9bd
x-dynamite-x <abdmoklss@gmail.com>
2025-11-09 05:16:18 +0300

    refactor: Simplify Reverb configuration in bootstrap.js
    - Updated Reverb connection settings to use fixed port values and enabled TLS by default.
    - Added a path configuration for Nginx compatibility.
    - Cleaned up code for better readability and maintainability.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,18 +8,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import Echo from "laravel-echo";
 import Pusher from 'pusher-js';
 
-
 const echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
+    path: '/reverb/app',  // مهم: يتوافق مع location /reverb في Nginx
 });
 
-// Export as a Vue plugin with install function
 export default {
   install(app) {
     app.config.globalProperties.$echo = echo;

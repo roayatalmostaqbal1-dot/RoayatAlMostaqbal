85769cd569e98438c3850db79ddf8c54be19e631
x-dynamite-x <abdmoklss@gmail.com>
2025-11-17 05:56:43 +0300

    feat: Restrict SuperAdmin routes to super-admin role
    - Updated the SuperAdmin route group to include a role middleware, ensuring that only users with the super-admin role can access the associated API resources for roles, permissions, and permissionRole.

--- routes/api/v1/SuperAdmin/api.php
+++ routes/api/v1/SuperAdmin/api.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Api\V1\TwoFactorAuthController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('SuperAdmin')->middleware(['auth:api'])->group(function () {
+Route::prefix('SuperAdmin')->middleware(['auth:api','role:super-admin'])->group(function () {
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('permissions', PermissionController::class);
     Route::apiResource('permissionRole', PermissionRoleController::class);
